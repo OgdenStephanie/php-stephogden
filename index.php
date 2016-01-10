2de6179a38d712e79305f2053710544829b5fbaf
Stephanie Ogden <stephogden4@gmail.com>
2016-01-10 13:04:27 -0700

    Index

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php echo '<p>Hello World My name is Steph</p>'; ?>
+        ?>
+    </body>
+</html>
