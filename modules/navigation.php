b060ad18605a93e769505d00e43c8ac1c6d0fe96
Stephanie Ogden <stephogden4@gmail.com>
2016-02-13 14:09:34 -0700

    again

--- modules/navigation.php
+++ modules/navigation.php
@@ -1,8 +1,8 @@
 
 <div> <ul>
-        <li><a href="/php/index.php/" class="button">Home</a></li>
-        <li><a href="/php/assignments/assignments.php/" class="button">Assignments</a></li>
-        <li><a href="/php/groupProjects/" class="button">Group Projects</a></li>
-        <li><a href="/php/teach.php/" class="button">Teach/Present</a></li>
+        <li><a href="php/index.php/" class="button">Home</a></li>
+        <li><a href="php/assignments/assignments.php/" class="button">Assignments</a></li>
+        <li><a href="php/groupProjects/" class="button">Group Projects</a></li>
+        <li><a href="php/teach.php/" class="button">Teach/Present</a></li>
 
     </ul>  </div>
