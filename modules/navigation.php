49a8e0bc8cad716093ede637628ab9e01bd437b7
Stephanie Ogden <stephogden4@gmail.com>
2016-02-13 14:06:29 -0700

    additions

--- modules/navigation.php
+++ modules/navigation.php
@@ -1,8 +1,8 @@
 
 <div> <ul>
-        <li><a href="/index.php/" class="button">Home</a></li>
-        <li><a href="/assignments/assignments.php/" class="button">Assignments</a></li>
-        <li><a href="/groupProjects/" class="button">Group Projects</a></li>
-        <li><a href="/teach.php/" class="button">Teach/Present</a></li>
-            
-    </ul>  </div> 
+        <li><a href="/php/index.php/" class="button">Home</a></li>
+        <li><a href="/php/assignments/assignments.php/" class="button">Assignments</a></li>
+        <li><a href="/php/groupProjects/" class="button">Group Projects</a></li>
+        <li><a href="/php/teach.php/" class="button">Teach/Present</a></li>
+
+    </ul>  </div>
